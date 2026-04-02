9d276657c7b6f9881259c566cb538fb0ba84fc5e
Jonathan <jonte.jonis@hotmail.com>
2026-04-02 14:36:35 +0200

    OrderRepo

---
+++ WebShop/Webshop.Domain/Interfaces/IOrderRepository.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Webshop.Domain.Entitites;
+
+namespace Webshop.Domain.Interfaces;
+
+public interface IOrderRepository
+{
+    Task<Order> GetByIdAsync(Guid id);
+    Task<List<Order>> GetAllAsync();
+    Task<List<Order>> GetOrdersByKundIdAsync(Guid kundId);
+    Task<List<Order>> GetOrdersByFraktOmbudIdAsync(Guid fraktOmbudId);
+    Task<List<Order>> GetByDatumSpannAsync(DateTime från, DateTime till);
+    Task<Order> FindByIdAsync(Guid id);
+    Task<Order> AddOrder(Order order);
+    Task<Order> UpdateOrder(Order order);
+    Task<Order> DeleteOrder(Guid id);
+    Task<bool> OrderExists(Guid id);
+
+}
---
+++ WebShop/Webshop.Infrastructure/EF/Repositories/OrderRepository.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Webshop.Domain.Entitites;
+using Webshop.Domain.Interfaces;
+
+namespace Webshop.Infrastructure.EF.Repositories;
+
+public class OrderRepository : IOrderRepository
+{
+    public Task<Order> AddOrder(Order order)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<Order> DeleteOrder(Guid id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<Order> FindByIdAsync(Guid id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<List<Order>> GetAllAsync()
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<List<Order>> GetByDatumSpannAsync(DateTime från, DateTime till)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<Order> GetByIdAsync(Guid id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<List<Order>> GetOrdersByFraktOmbudIdAsync(Guid fraktOmbudId)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<List<Order>> GetOrdersByKundIdAsync(Guid kundId)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<bool> OrderExists(Guid id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<Order> UpdateOrder(Order order)
+    {
+        throw new NotImplementedException();
+    }
+}
