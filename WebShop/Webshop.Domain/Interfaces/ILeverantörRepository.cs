1c1da88dba93ea9ac840c1c45d63d5d91a70bb3a
Jonathan <jonte.jonis@hotmail.com>
2026-04-02 14:02:59 +0200

    Added Repo for Leverantör

---
+++ WebShop/Webshop.Domain/Interfaces/ILeverantörRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Webshop.Domain.Entitites;
+
+namespace Webshop.Domain.Interfaces;
+
+public interface ILeverantörRepository
+{
+    Task<Leverantör> GetByIdAsync(Guid id);
+    Task<List<Leverantör>> GetAllAsync();
+    Task<Leverantör> AddAsync(Leverantör leverantör);
+    Task<Leverantör> DeleteAsync(Guid id);
+    Task<Leverantör> UpdateAsync(Leverantör leverantör);
+    Task<bool> ExistsAsync(Guid id);
+}
---
+++ WebShop/Webshop.Infrastructure/EF/Repositories/LeverantörRepository.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Webshop.Domain.Entitites;
+using Webshop.Domain.Interfaces;
+
+namespace Webshop.Infrastructure.EF.Repositories;
+
+public class LeverantörRepository : ILeverantörRepository
+{
+    public Task<Leverantör> AddAsync(Leverantör leverantör)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<Leverantör> DeleteAsync(Guid id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<bool> ExistsAsync(Guid id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<List<Leverantör>> GetAllAsync()
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<Leverantör> GetByIdAsync(Guid id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<Leverantör> UpdateAsync(Leverantör leverantör)
+    {
+        throw new NotImplementedException();
+    }
+}
