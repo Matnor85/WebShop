1f03b34f9b95819735956f56c7703780d9a6c2e4
Mathias <Mathias.norrbacken@gmail.com>
2026-04-07 12:02:29 +0200

    IFraktOmbudService and FraktOmbudService added

---
+++ WebShop/Webshop.Application/Interfaces/IFraktOmbudService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Webshop.Domain.Entitites;
+
+namespace Webshop.Application.Interfaces;
+
+internal interface IFraktOmbudService
+{
+    Task<FraktOmbud> GetByIdAsync(Guid id);
+    Task<List<FraktOmbud>> GetAllAsync();
+    Task<FraktOmbud> AddAsync(FraktOmbud fraktOmbud);
+    Task UpdateAsync(FraktOmbud fraktOmbud);
+    Task DeleteAsync(Guid id);
+    Task<bool> ExistAsync(Guid id);
+}
