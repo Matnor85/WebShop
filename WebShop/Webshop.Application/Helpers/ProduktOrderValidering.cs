ca6497f1dc064e82ef89579150ee029a9ce68b65
Jonathan <jonte.jonis@hotmail.com>
2026-04-07 15:14:05 +0200

    la till mer validering

---
+++ WebShop/Webshop.Application/Helpers/ProduktOrderValidering.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Webshop.Application.Helpers;
+
+public class ProduktOrderValidering
+{
+    public static void ValidateAntal(int antal)
+    {
+        if (antal <= 0)
+        {
+            throw new ArgumentException("Antal måste vara större än noll.");
+        }
+    }
+
+    public static void ValidateLager(int antal,int lagerAntal )
+    {
+        if (antal > lagerAntal)
+            throw new ArgumentException("Otillräckligt lagersaldo");
+    }
+}
