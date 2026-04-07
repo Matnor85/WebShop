5613e6a55c0c5feccd06b058d6f1d3523df2f300
Mathias <Mathias.norrbacken@gmail.com>
2026-04-07 14:51:38 +0200

    -

---
+++ WebShop/Webshop.Application/Helpers/LeverantörValendering.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Webshop.Domain.Entitites;
+
+namespace Webshop.Application.Helpers;
+
+public class LeverantörValendering
+{
+    public static void IdValendering(Leverantör leverantör)
+    {
+        if (leverantör.Id == Guid.Empty)
+        {
+            leverantör.Id = Guid.NewGuid();
+        }
+    }
+
+    public static void NamnValendering(Leverantör leverantör)
+    {
+        if (string.IsNullOrWhiteSpace(leverantör.Namn))
+        {
+            throw new ArgumentException("Namn får inte vara tomt.");
+        }
+    }
+}
