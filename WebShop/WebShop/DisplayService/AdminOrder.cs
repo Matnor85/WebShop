b092e511a0d49b51a897ab5990d29c31eca02b30
Jonathan <jonte.jonis@hotmail.com>
2026-04-09 12:11:19 +0200

    Added order

--- WebShop/WebShop/DisplayService/AdminOrder.cs
+++ WebShop/WebShop/DisplayService/AdminOrder.cs
@@ -1,10 +1,36 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using Webshop.Application.Interfaces;
 
 namespace WebShop.Presentation.DisplayService;
 
 public class AdminOrder
 {
+    IOrderService _orderService;
+    IKundService _kundService;
+    public AdminOrder(IOrderService orderService, IKundService kundService)
+    {
+        _orderService = orderService;
+        _kundService = kundService;
+    }
+    public async Task ShowOrderList()
+    {
+        try
+        {
+            var orders = await _orderService.GetAllAsync();
+            if (orders == null)
+                throw new ArgumentException("Inga order hittades.");
+            for (int i = 0; i < orders.Count; i++)
+            {
+                Console.WriteLine($"Id {i + 1}, Kund: {orders[i].Kund.Namn}, Totalpris: {orders[i].TotalPris}, Fraktombud: {orders[i].FraktOmbud.Namn}");
+            }
+        }
+        catch (ArgumentException ex)
+        {
+            Console.WriteLine($"Fel: {ex.Message}");
+        }
+    }
 
+    
 }
--- WebShop/WebShop/Menu/Submenu/OrderMenu.cs
+++ WebShop/WebShop/Menu/Submenu/OrderMenu.cs
@@ -1,9 +1,50 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using WebShop.Presentation.DisplayService;
 
 namespace WebShop.Presentation.Menu.Submenu;
 
-internal class OrderMenu
+public class OrderMenu(AdminOrder _adminOrder)
 {
+    bool _isRunning = true;
+
+    public void ShowAdminOrderMenu()
+    {
+        Console.Clear();
+        Console.WriteLine("Hantera order!");
+        Console.WriteLine("1 - Visa alla order");
+        Console.WriteLine("2 - Skapa ny order");
+        Console.WriteLine("3 - Uppdatera order");
+        Console.WriteLine("4 - Ta bort order");
+        Console.WriteLine("5 - Tillbaka till huvudmenyn");
+    }
+
+    public async Task HanteraOrderAsync()
+    {
+        Console.Clear();
+        ShowAdminOrderMenu();
+        var input = Console.ReadLine();
+        switch (input)
+        {
+            case "1":
+                Console.Clear();
+                await _adminOrder.ShowOrderList();
+                break;
+            case "5":
+                _isRunning = false;
+                break;
+            default:
+                Console.WriteLine("Ogiltigt val, försök igen.");
+                break;
+        }
+    }
+
+    public async Task OrderMenuRunAsync()
+    {
+        while (_isRunning)
+        {
+            await HanteraOrderAsync();
+        }
+    }
 }
