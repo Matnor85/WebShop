ec9d7232e64d5dff4b6898ce114b727d6c417e42
Jonathan <jonte.jonis@hotmail.com>
2026-04-13 11:50:04 +0200

    corrected config

--- WebShop/Webshop.Infrastructure/EF/Configurations/ProduktKampanjConfiguration.cs
+++ WebShop/Webshop.Infrastructure/EF/Configurations/ProduktKampanjConfiguration.cs
@@ -13,6 +13,10 @@ public class ProduktKampanjConfiguration : IEntityTypeConfiguration<ProduktKampa
     {
         builder.HasKey(pk => pk.Id);
 
+        builder.Property(pk => pk.Rabatt)
+               .HasColumnType("decimal(5,2)")
+               .IsRequired();
+
         builder.HasOne(pk => pk.Produkt)
                .WithMany()
                .HasForeignKey(pk => pk.ProduktId)
