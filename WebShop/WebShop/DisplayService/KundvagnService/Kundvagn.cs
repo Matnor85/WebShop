cfb4185e53f5619a14f2cebc87e0d2f9290aedc8
Jonathan <jonte.jonis@hotmail.com>
2026-04-13 20:20:38 +0200

    Created 2 classes with methods, add remove and clear

---
+++ WebShop/WebShop/DisplayService/KundvagnService/Kundvagn.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Webshop.Domain.Entitites;
+using WebShop.Presentation.DisplayService.KundvagnService;
+
+namespace WebShop.Presentation.DisplayService.Models;
+
+public class Kundvagn
+{
+    public List<KundvagnItem> Items { get; set; } = new();
+    public decimal TotalSumma => Items.Sum(i => i.Delsumma);
+
+
+    public void LäggTill(Produkt produkt, int antal)
+    {
+        var existingItem = Items.FirstOrDefault(i => i.Produkt.Id == produkt.Id);
+        if (existingItem != null)
+        {
+            existingItem.Antal += antal;
+        }
+        else
+        {
+            Items.Add(new KundvagnItem { Produkt = produkt, Antal = antal, PrisVidKöp = produkt.Pris });
+        }
+    }
+
+    public void TaBort(Guid produktId) => Items.RemoveAll(i => i.Produkt.Id == produktId);
+
+    public void TömVagn() => Items.Clear();
+}
