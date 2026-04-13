852a17392d25e26c3e3153b408876228835306b5
Jonathan <jonte.jonis@hotmail.com>
2026-04-13 13:32:35 +0200

    added folder and classes for seperate injections

---
+++ WebShop/WebShop/Extensions/PresentationExtensions.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using WebShop.Presentation.DisplayService;
+using WebShop.Presentation.DisplayService.AdminService;
+using WebShop.Presentation.Menu;
+using WebShop.Presentation.Menu.Shop_Submenu;
+using WebShop.Presentation.Menu.Submenu;
+
+namespace WebShop.Presentation.Extensions;
+
+public static class PresentationExtensions
+{
+    public static IServiceCollection AddMenus(this IServiceCollection services)
+    {
+        //meny
+        services.AddScoped<Meny>();
+        services.AddScoped<WebShopMenu>();
+        services.AddScoped<AdminMenu>();
+        services.AddScoped<KategoriMenu>();
+        services.AddScoped<ProduktMenu>();
+        services.AddScoped<KundMenu>();
+        services.AddScoped<OrderMenu>();
+        // Webbshop meny
+        services.AddScoped<ShoppingCartMenu>();
+        services.AddScoped<SearchProductMenu>();
+        services.AddScoped<BrowseCategoriesMenu>();
+        services.AddScoped<ManageOrderHistoryMenu>();
+        services.AddScoped<KampanjerMenu>();
+
+        //displayservice
+        //Admin
+        services.AddScoped<AdminKund>();
+        services.AddScoped<AdminProdukt>();
+        services.AddScoped<AdminKategori>();
+        services.AddScoped<AdminOrder>();
+        services.AddScoped<AdminKampanj>();
+        //Shop
+        services.AddScoped<ShopBrowseCategories>();
+        services.AddScoped<ShopSearchProduct>();
+        services.AddScoped<ShopShoppingCart>();
+        services.AddScoped<ShopManageOrderHistory>();
+        services.AddScoped<ShopKampanj>();
+        return services;
+    }
+}
---
+++ WebShop/Webshop.Infrastructure/Extensions/InfrastructureExtensions.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Webshop.Domain.Interfaces;
+using Webshop.Infrastructure.EF.Repositories;
+
+namespace Webshop.Infrastructure.Extensions;
+
+public static class InfrastructureExtensions
+{
+    public static IServiceCollection AddRepositories(this IServiceCollection services)
+    {
+        services.AddScoped<IProduktRepository, ProduktRepository>();
+        services.AddScoped<IProduktOrderRepository, ProduktOrderRepository>();
+        services.AddScoped<IKategoriRepository, KategoriRepository>();
+        services.AddScoped<IKundRepository, KundRepository>();
+        services.AddScoped<ILeverantörRepository, LeverantörRepository>();
+        services.AddScoped<IFraktOmbudRepository, FraktOmbudRepository>();
+        services.AddScoped<IOrderRepository, OrderRepository>();
+        services.AddScoped<IProduktKampanjRepository, ProduktKampanjRepository>();
+
+        return services;
+    }
+}
