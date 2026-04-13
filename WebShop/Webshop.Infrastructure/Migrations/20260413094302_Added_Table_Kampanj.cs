c5b415c497b771e2b28338cc29d15d8f6f33b3a7
Jonathan <jonte.jonis@hotmail.com>
2026-04-13 11:43:51 +0200

    added migration for kampanj table

---
+++ WebShop/Webshop.Infrastructure/Migrations/20260413094302_Added_Table_Kampanj.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Webshop.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class Added_Table_Kampanj : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "ProduktKampanjer",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    ProduktId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Rabatt = table.Column<decimal>(type: "decimal(18,2)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ProduktKampanjer", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_ProduktKampanjer_Produkter_ProduktId",
+                        column: x => x.ProduktId,
+                        principalTable: "Produkter",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ProduktKampanjer_ProduktId",
+                table: "ProduktKampanjer",
+                column: "ProduktId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ProduktKampanjer");
+        }
+    }
+}
