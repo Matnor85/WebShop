9259f0bede7135399fa12bf4206f8a273b7f8952
Jonathan <jonte.jonis@hotmail.com>
2026-04-15 12:09:19 +0200

    ReFactory class

--- WebShop/WebShop/DisplayService/ShopService/ShopSearchProduct.cs
+++ WebShop/WebShop/DisplayService/ShopService/ShopSearchProduct.cs
@@ -1,9 +1,73 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using Webshop.Application.Interfaces;
+using Webshop.Domain.Entitites;
+using Webshop.Infrastructure.EF;
+using WebShop.Presentation.DisplayService.ValutaApi;
+using WebShop.Presentation.Menu;
 
 namespace WebShop.Presentation.DisplayService.ShopService;
 
-public class ShopSearchProduct
+public class ShopSearchProduct(IProduktService produktService, ValutaSession valutaSession)
 {
+    bool _isRunning = true;
+
+    public async Task SearchForProduct()
+    {
+        Console.Clear();
+        Console.Write("=== Sök efter produkt ===\n[Esc] - Tillbaka\nNamn: ");
+        ConsoleKeyInfo key = Console.ReadKey(true);
+        
+        if (key.Key == ConsoleKey.Escape)
+        {
+            return;
+        }
+        
+        var products = (await produktService.GetAllAsync())
+            .Where(p => p.Namn != null && p.Namn.Contains(key.KeyChar.ToString()))
+            .ToList();
+
+        InputCheck(key, products);
+    }
+
+    private void InputCheck(ConsoleKeyInfo key, List<Produkt> products)
+    {
+        Console.Clear();
+        if (products == null || products.Count == 0)
+        {
+            Console.WriteLine("Ingen produkt hittades.");
+            Meny.Wait();
+            return;
+        }
+
+        Meny.CreateLines('-', 90);
+        DisplayProducts(products);
+
+        Meny.Wait();
+    }
+
+    private void DisplayProducts(List<Produkt> products)
+    {
+        foreach (var product in products)
+        {
+            Console.WriteLine($"Namn: {product.Namn}");
+            Console.WriteLine($"Pris: {valutaSession.FormatPris(product.Pris)}");
+            Console.WriteLine($"Antal i lager: {product.LagerAntal}");
+            Console.WriteLine($"Kategori: {product.Kategori?.Namn}");
+            Console.WriteLine($"Färg: {product.Färg}");
+            Console.WriteLine($"Storlek: {product.Storlek}");
+            Meny.CreateLines('-', 90);
+        }
+    }
+
+    public async Task SearchProductRun()
+    {
+        _isRunning = true;
+        while (_isRunning)
+        {
+            await SearchForProduct();
+            break;
+        }
+    }
 }
